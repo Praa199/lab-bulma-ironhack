434708beedd3f6b86eb9aac6d993a8ab46bce6df
Aldo <aldo.pena.93@hotmail.com>
2021-05-02 20:07:00 +0200

    done iteration 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
+        <section>
+          <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+          <FormField
+            label="Email"
+            type="email"
+            placeholder="e.g. alexsmith@gmail.com"
+          />
+        </section>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
